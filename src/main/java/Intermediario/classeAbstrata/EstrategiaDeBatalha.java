a9edafe071f04eeb3b36a021f7589c8a9d80d9c8
Felipe <felipe.rcardoso@embraer.com.br>
2025-03-13 14:14:40 -0300

    Comentando final dentro de uma interface

--- src/main/java/Intermediario/classeAbstrata/EstrategiaDeBatalha.java
+++ src/main/java/Intermediario/classeAbstrata/EstrategiaDeBatalha.java
@@ -2,6 +2,11 @@ package Intermediario.classeAbstrata;
 
 public interface EstrategiaDeBatalha {
 
+    // Por padrão, antes deste String, tem um "final" mas não é necessário colocar ele
+    // Havendo um final, não é possível mudar o valor ali ou dar um override!
+    // Como se fosse uma variável const do Java Script
+    final String nome = "Estrategia Nevoa";
+
      // Preciso instanciar ele obrigatoriamente em todo lugar que eu implementar essa interface
     void estrategiaDeBatalhaNinja();
 
