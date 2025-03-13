451445d8660f5bdef42c0a88bca89a50b0cb0ee0
Felipe <felipe.rcardoso@embraer.com.br>
2025-03-13 09:21:04 -0300

    Metodo toString explicação

--- src/main/java/Intermediario/classeAbstrata/Main.java
+++ src/main/java/Intermediario/classeAbstrata/Main.java
@@ -49,7 +49,8 @@ public class Main {
         minato.inteligenciaDeCombate();
         minato.inteligenciaDeCombate(160);
 
-        System.out.println(minato);
-
+        System.out.println(minato); // Quando eu faço isso aqui eu estou acessando a referência da minha memória e não necessariamente o valor que existe nela
+        // Por isso existe o método to string em que eu posso dar um override no método padrão do java para retornar outra coisa
+        // É recomendado que sempre em nossas classes haja um método toString() sobreposto ao normal!
     }
 }
