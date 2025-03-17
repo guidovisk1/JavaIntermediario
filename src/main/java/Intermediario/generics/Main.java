0b06b7432bd9ff24ca1382fd79aa94e45c582543
Felipe <felipe.rcardoso@embraer.com.br>
2025-03-17 14:31:51 -0300

    Generics

---
+++ src/main/java/Intermediario/generics/Main.java
@@ -0,0 +1,16 @@
+package Intermediario.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        EquipamentosNinja equipamentosNinja = new EquipamentosNinja("Kunai de ferro");
+        EquipamentosNinja equipamentosNinja2 = new EquipamentosNinja("Pergaminho");
+        EquipamentosNinja equipamentosNinja3 = new EquipamentosNinja("Shuriken");
+
+        BolsaGenerica<EquipamentosNinja> bolsaGenerica = new BolsaGenerica<>();
+        bolsaGenerica.adicionarEquipamento(equipamentosNinja);
+        bolsaGenerica.adicionarEquipamento(equipamentosNinja2);
+        bolsaGenerica.adicionarEquipamento(equipamentosNinja3);
+
+        System.out.println(bolsaGenerica);
+    }
+}
